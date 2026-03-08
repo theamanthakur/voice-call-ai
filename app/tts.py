8b45bb779dd9349b4b08caaf47eabbc2937d843b
Aman Thakur <amanthakur@Amans-MacBook-Air.local>
2026-03-08 15:40:03 +0530

    LLM  TTS tweak 2

--- app/tts.py
+++ app/tts.py
@@ -90,7 +90,7 @@ from app.config import ELEVENLABS_API_KEY
 
 client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
 
-VOICE_ID = "1zUSi8LeHs9M2mV8X6YS"
+VOICE_ID = "Zjj2iX3aHYDcJSG4mMzk"
 MODEL_ID = "eleven_turbo_v2_5"
 VOICE_SETTINGS = VoiceSettings(
     stability=0.30,

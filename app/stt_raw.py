4cad4e59d2df8005c63aed8c89801bc98d9f99fb
Aman Thakur <amanthakur@Amans-MacBook-Air.local>
2026-05-27 19:01:26 +0530

    stt and lmm 1

--- app/stt_raw.py
+++ app/stt_raw.py
@@ -32,24 +32,14 @@ class DeepgramRaw:
         #     f"&interim_results=true"
         #     f"&endpointing=100"
         # )
-        # params = (
-        #     f"model=nova-2"
-        #     f"&encoding=mulaw"
-        #     f"&sample_rate=8000"
-        #     f"&interim_results=true"
-        #     f"&endpointing=100"
-        # )
-
         params = (
-    f"model=nova-2"
-    f"&language=hi-IN"
-    f"&smart_format=true"
-    f"&punctuate=true"
-    f"&encoding=mulaw"
-    f"&sample_rate=8000"
-    f"&interim_results=true"
-    f"&endpointing=150"
-    )
+            f"model=nova-2"
+            f"&language={LANGUAGE}"
+            f"&encoding=mulaw"
+            f"&sample_rate=8000"
+            f"&interim_results=true"
+            f"&endpointing=100"
+        )
         uri = f"wss://api.deepgram.com/v1/listen?{params}"
 
         headers = [
